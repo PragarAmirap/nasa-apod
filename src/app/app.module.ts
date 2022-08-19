daac76a16c9f0d91ae2e764ea2e802554123e040
PragarAmirap <92669576+PragarAmirap@users.noreply.github.com>
2022-08-19 09:54:09 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http'
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -8,7 +9,7 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { NavVarComponent } from './main_page/nav-var/nav-var.component';
 import { FooterComponent } from './main_page/footer/footer.component';
-import {FormsModule} from '@angular/forms'
+import { FormsModule } from '@angular/forms'
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import {FormsModule} from '@angular/forms'
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
