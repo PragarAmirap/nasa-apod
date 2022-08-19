cf6e53a8aca845e17bc2d12bc986cf50b3e35c14
PragarAmirap <92669576+PragarAmirap@users.noreply.github.com>
2022-08-19 09:54:44 +0530

    changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
-import {HttpClient} from '@angular/common/http'
+import { HttpClient } from '@angular/common/http'
 import { formatDate } from '@angular/common';
+
 
 @Component({
   selector: 'app-home',
@@ -9,32 +10,34 @@ import { formatDate } from '@angular/common';
 })
 export class HomeComponent implements OnInit {
 
-  date='';
-  urlForFetch:string=''
-  apiurl: string='https://api.nasa.gov/planetary/apod?api_key=DEFyLIrqem0iApZ07nObhyOeoos6JrEISMy5afFR'
+  date = '';
+  urlForFetch: string = ''
+  apiurl: string = 'https://api.nasa.gov/planetary/apod?api_key=DEFyLIrqem0iApZ07nObhyOeoos6JrEISMy5afFR'
   data: any;
-  todayDate:string='';
-  constructor(private http:HttpClient) { }
+  todayDate: string = '';
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.getData();
-    this.todayDate= formatDate(new Date(), 'yyyy-MM-dd', 'en-IN')
+    this.todayDate = formatDate(new Date(), 'yyyy-MM-dd', 'en-IN');
   }
-  
-  getData(){
-    let url= "https://api.nasa.gov/planetary/apod?api_key=DEFyLIrqem0iApZ07nObhyOeoos6JrEISMy5afFR";
-    if(this.date !=''){
-    this.http.get(this.urlForFetch= this.apiurl+'&date='+this.date).subscribe(
-      response =>{
-        console.log(response);
-        this.data = response;
-      }
-    );}else{
-      this.http.get(url).subscribe(
-        response =>{
+
+  getData() {
+    let url = "https://api.nasa.gov/planetary/apod?api_key=DEFyLIrqem0iApZ07nObhyOeoos6JrEISMy5afFR"
+    if (this.date != '') {
+      this.http.get(this.urlForFetch = this.apiurl + '&date=' + this.date).subscribe(
+        response => {
           console.log(response);
           this.data = response;
         }
-      );}
+      );
+    } else {
+      this.http.get(url).subscribe(
+        response => {
+          console.log(response);
+          this.data = response;
+        }
+      );
+    }
   };
 }
